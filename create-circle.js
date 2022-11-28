8ac873d48168030666bd901d9a058031d90eff01
Andreas Grasser <andi@grasserisen.de>
2022-11-28 18:19:11 +0100

    Bugfix for Download Function

--- create-circle.js
+++ create-circle.js
@@ -196,8 +196,17 @@ function httpRequest(url, callback, callbackVal=null)
 }
 
 function downloadImage(){
-    var link = document.createElement('a');
-    link.download = 'mastodon-circle.png';
-    link.href = document.getElementById('canvas').toDataURL()
-    link.click();
+    var lnk = document.createElement('a'), e;
+    lnk.download = 'mastodon-circle.png';
+    lnk.href = canvas.toDataURL("image/png;base64");
+
+    if (document.createEvent) {
+        e = document.createEvent("MouseEvents");
+        e.initMouseEvent("click", true, true, window,
+                        0, 0, 0, 0, 0, false, false, false,
+                        false, 0, null);
+        lnk.dispatchEvent(e);
+    } else if (lnk.fireEvent) {
+        lnk.fireEvent("onclick");
+    }
   }
